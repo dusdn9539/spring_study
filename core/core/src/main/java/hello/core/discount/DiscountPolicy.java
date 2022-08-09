a35c6e33e157e2508cbea7717447b4ef80fce0b3
dusdn9539 <dusdn9539@naver.com>
2022-08-09 16:39:18 +0900

    first upload

---
+++ core/core/src/main/java/hello/core/discount/DiscountPolicy.java
@@ -0,0 +1,11 @@
+package hello.core.discount;
+
+import hello.core.member.Member;
+
+public interface DiscountPolicy {
+
+   /*
+   * @retrun 할인 대상금액
+   * */
+    int discount(Member member, int price);
+}
